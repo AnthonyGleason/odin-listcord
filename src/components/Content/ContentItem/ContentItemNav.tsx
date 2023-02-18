4f1f9a712463a0f299a793b4ccaa6722e5224c2b
Anthony Infortunio <contact@anthonyinfortun.io>
2023-02-18 16:51:54 -0500

    add margins and padding, additional positional

--- src/components/Content/ContentItem/ContentItemNav.tsx
+++ src/components/Content/ContentItem/ContentItemNav.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ContentItemNav(){
   return(
     <div className='content-item-nav'>
-        <div className="content-item-origin">Post Origin</div>
+        <div className="content-item-origin">Post Community</div>
         <div className="content-item-date">02/17/2023</div>
         <button className="content-join-button">Join This Community</button>
     </div>
